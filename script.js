accd7bdcd5efb1464f62923612c6945856d5698d
GadongKarlSaid <146708073+GadongKarlSaid@users.noreply.github.com>
2023-10-04 14:49:44 +0800

    feat: This is my Activity 2 in DSA

---
+++ script.js
@@ -0,0 +1,13 @@
+//Activity 2
+
+let firstFavNum = 14;
+let secondFavNum = 100;
+
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum != secondFavNum);
